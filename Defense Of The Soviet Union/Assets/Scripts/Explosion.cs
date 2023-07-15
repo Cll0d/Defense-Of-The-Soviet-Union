3f5371e7182539a4668d8520aa72b5ab2bb0ced7
Cll0d <y_trofimov@bk.ru>
2023-07-15 13:55:12 +0500

    Миномет работает

---
+++ Defense Of The Soviet Union/Assets/Scripts/Explosion.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] float _damage;
+
+    public float Radius;
+    public float Force;
+
+    public GameObject ExplosionEffect;
+
+    void Update()
+    {
+
+    }
+
+    private void Explode()
+    {
+        Collider[] overlappedColliders = Physics.OverlapSphere(transform.position, Radius);// Все коллайдеры которые попали в сферу помещаются в массив
+
+
+        for (int i = 0; i < overlappedColliders.Length; i++)
+        {
+            Rigidbody rigidbody = overlappedColliders[i].attachedRigidbody;
+            if (rigidbody)
+            {
+                rigidbody.AddExplosionForce(Force, transform.position, Radius);
+            }
+
+            Enemy enemy = overlappedColliders[i].GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(_damage);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Doroga")
+        {
+            Explode();
+            Destroy(gameObject);
+            Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+            Debug.Log("Хуйнуло");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+}
