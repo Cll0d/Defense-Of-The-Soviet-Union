29f838c8c20a2d8063d6f503ac859d736354d963
Cll0d <y_trofimov@bk.ru>
2023-09-17 15:34:19 +0500

    Зомби заебись

--- Defense Of The Soviet Union/Assets/Scripts/Enemy.cs
+++ Defense Of The Soviet Union/Assets/Scripts/Enemy.cs
@@ -9,8 +9,15 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _damage;
     [SerializeField] private int _coinForKill;
     [SerializeField] private CoinManager _coinManager;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+=======
+>>>>>>> Stashed changes
     [SerializeField] private AudioSource _audioSource;
     
+>>>>>>> 24bf2a693cde8a8c482608ae2ce2e22fbddea287
     private Animator _animator;
     private HealthBase _healthBase;
     private bool _isAttack = false;
@@ -22,20 +29,20 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if(!_isAttack)
-        {
-            _animator.SetBool(name: "Atack", value: false); 
-            
-        }
-        else
-        {
-            _animator.SetBool(name: "Atack", value: true);
-        }
+        //if (!_isAttack)
+        //{
+        //    _animator.SetBool(name: "Atack", value: false);
+
+        //}
+        //else
+        //{
+        //    _animator.SetBool(name: "Atack", value: true);
+        //}
     }
     public void TakeDamage(float damage)
     {
-        _health -= damage * (1 -  (_armor / 100));
-        if(_health < 1)
+        _health -= damage * (1 - (_armor / 100));
+        if (_health < 1)
         {
             _animator.SetBool(name: "Die", value: true);
             _audioSource.Play();
@@ -50,12 +57,17 @@ public class Enemy : MonoBehaviour
     }
     private IEnumerator Attack()
     {
-        if(_healthBase.Health > 0)
+        if (_healthBase.Health > 0)
         {
             yield return new WaitForSeconds(1);
             _healthBase.TakeDamage(_damage);
             StartCoroutine(Attack());
 
+        }
+        else
+        {
+            _animator.SetBool(name: "Walk", value: true); 
+            _animator.SetBool(name: "Atack", value: false);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -66,10 +78,12 @@ public class Enemy : MonoBehaviour
             _healthBase = healthBase;
             _isAttack = true;
             StartCoroutine(Attack());
+            _animator.SetBool(name: "Atack", value: true);
         }
         else
         {
             _isAttack = false;
+                _animator.SetBool(name: "Walk", value: true);
         }
     }
 }
